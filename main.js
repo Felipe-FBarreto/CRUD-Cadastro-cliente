a58caf8d94027906285c5104aa54c96f30f2adb8
Felipe Barreto <felipe.devfrontend@gmail.com>
2021-10-02 15:59:55 -0300

    criado as funcoes crud

--- main.js
+++ main.js
@@ -6,7 +6,37 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+// Crud - Create Read Update Delete
 
+const tempClient ={
+    nome: 'felipe',
+    email: 'felipebarreto1050@gmail.com',
+    celular: '(17) 99674-9148',
+    cidade: 'Catanduva'
+}
+
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
+const getLocalStorage = () => JSON.parse(localStorage.getItem("dbClient")) ?? []
+
+const deletar = (index) => {
+    const dadoBanco = readClient()
+    dadoBanco.splice(index,1)
+    setLocalStorage(dadoBanco)
+}
+
+const updateClient = (index , client) =>{
+    const dadosClient = readClient()
+    dadosClient[index] = client
+    setLocalStorage(dadosClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const creatClient = (client)=>{
+    const dados = getLocalStorage()
+    dados.push(client)
+    setLocalStorage(dados)   
+}
 
 // EVENTOS
 document.getElementById('cadastrarCliente')
